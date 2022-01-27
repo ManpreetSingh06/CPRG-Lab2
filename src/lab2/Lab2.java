a6773332be9c94be4fcd8dd51d9cb98c4349d68e
manpreet <manpreet@Manpreet>
2022-01-26 18:00:16 -0800

    New Project Creation Updated lab2 to push successfull

--- src/lab2/Lab2.java
+++ src/lab2/Lab2.java
@@ -16,7 +16,7 @@ public class Lab2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Lab2, Second Push ");
+        System.out.println("Lab2,Hello world Push ");
     }
     
 }
